5da3b3c07490d9a032f57fc1bef9cd33df634134
Ridhwan Nashir <ridhwanashir@gmail.com>
2021-06-24 20:54:28 +0700

    Finalizing Database

--- Database/walkwalk.sql
+++ Database/walkwalk.sql
@@ -1,135 +1,294 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.24 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2021 at 03:53 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table walkwalk.admin
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Database: `walkwalk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL DEFAULT '0',
-  `password` varchar(255) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- Dumping data for table walkwalk.admin: ~0 rows (approximately)
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-	(1, 'admin', 'admin');
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-
--- Dumping structure for table walkwalk.bookhotel
-CREATE TABLE IF NOT EXISTS `bookhotel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `PenginapanID` int(11) NOT NULL DEFAULT '0',
-  `UserID` int(11) NOT NULL DEFAULT '0',
-  `TotalHarga` int(11) NOT NULL DEFAULT '0',
-  `Durasi` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `password` varchar(255) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.bookhotel: ~0 rows (approximately)
-/*!40000 ALTER TABLE `bookhotel` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bookhotel` ENABLE KEYS */;
+--
+-- Dumping data for table `admin`
+--
 
--- Dumping structure for table walkwalk.kamar
-CREATE TABLE IF NOT EXISTS `kamar` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookhotel`
+--
+
+CREATE TABLE `bookhotel` (
+  `id` int(11) NOT NULL,
+  `kamar_id` int(11) NOT NULL DEFAULT 0,
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `total_harga` int(11) NOT NULL DEFAULT 0,
+  `durasi` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kamar`
+--
+
+CREATE TABLE `kamar` (
+  `id` int(11) NOT NULL,
   `penginapan_id` int(11) NOT NULL,
   `no_kamar` int(11) NOT NULL,
-  `harga` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.kamar: ~0 rows (approximately)
-/*!40000 ALTER TABLE `kamar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kamar` ENABLE KEYS */;
+--
+-- Dumping data for table `kamar`
+--
 
--- Dumping structure for table walkwalk.membayar
-CREATE TABLE IF NOT EXISTS `membayar` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserID` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+INSERT INTO `kamar` (`id`, `penginapan_id`, `no_kamar`, `harga`) VALUES
+(1, 1, 101, 350000),
+(2, 1, 102, 360000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id` int(11) NOT NULL,
+  `tiket_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `total_harga` int(11) NOT NULL DEFAULT 0,
+  `metode_pembayaran` int(11) NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `foto_bukti` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.membayar: ~0 rows (approximately)
-/*!40000 ALTER TABLE `membayar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `membayar` ENABLE KEYS */;
+--
+-- Dumping data for table `pembayaran`
+--
 
--- Dumping structure for table walkwalk.pembayaran
-CREATE TABLE IF NOT EXISTS `pembayaran` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `TiketID` int(11) NOT NULL DEFAULT '0',
-  `TotalHarga` int(11) NOT NULL DEFAULT '0',
-  `MetodePembayaran` varchar(50) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `pembayaran` (`id`, `tiket_id`, `book_id`, `total_harga`, `metode_pembayaran`, `status`, `foto_bukti`) VALUES
+(1, 1, NULL, 2000000, 1, 2, '3401961jaya jaya jaya.jpg');
 
--- Dumping data for table walkwalk.pembayaran: ~0 rows (approximately)
-/*!40000 ALTER TABLE `pembayaran` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pembayaran` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Dumping structure for table walkwalk.penginapan
-CREATE TABLE IF NOT EXISTS `penginapan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `penginapan`
+--
+
+CREATE TABLE `penginapan` (
+  `id` int(11) NOT NULL,
   `nama_tempat` varchar(255) NOT NULL DEFAULT '0',
   `alamat` varchar(255) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.penginapan: ~0 rows (approximately)
-/*!40000 ALTER TABLE `penginapan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `penginapan` ENABLE KEYS */;
+--
+-- Dumping data for table `penginapan`
+--
 
--- Dumping structure for table walkwalk.tempat_wisata
-CREATE TABLE IF NOT EXISTS `tempat_wisata` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+INSERT INTO `penginapan` (`id`, `nama_tempat`, `alamat`, `foto`) VALUES
+(1, 'Sun in Pangandaran', 'Pangandaran', '6752904sun in pangandaran.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tempat_wisata`
+--
+
+CREATE TABLE `tempat_wisata` (
+  `id` int(11) NOT NULL,
   `nama_tempat` varchar(255) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
   `harga` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.tempat_wisata: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tempat_wisata` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tempat_wisata` ENABLE KEYS */;
+--
+-- Dumping data for table `tempat_wisata`
+--
 
--- Dumping structure for table walkwalk.tiketwisata
-CREATE TABLE IF NOT EXISTS `tiketwisata` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserID` int(11) NOT NULL DEFAULT '0',
-  `WisataID` int(11) NOT NULL DEFAULT '0',
-  `TotalTiket` int(11) NOT NULL DEFAULT '0',
-  `TotalHarga` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+INSERT INTO `tempat_wisata` (`id`, `nama_tempat`, `alamat`, `harga`, `foto`) VALUES
+(1, 'Dufan', 'Ancol', 200000, '4617273dufan.jpg'),
+(2, 'Pantai Pangandaran', 'Pangandaran', 50000, '7004461pangandaran.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiketwisata`
+--
+
+CREATE TABLE `tiketwisata` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `wisata_id` int(11) NOT NULL DEFAULT 0,
+  `total_tiket` int(11) NOT NULL DEFAULT 0,
+  `total_harga` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.tiketwisata: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tiketwisata` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tiketwisata` ENABLE KEYS */;
+--
+-- Dumping data for table `tiketwisata`
+--
 
--- Dumping structure for table walkwalk.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+INSERT INTO `tiketwisata` (`id`, `user_id`, `wisata_id`, `total_tiket`, `total_harga`) VALUES
+(1, 2, 1, 10, 2000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '0',
   `password` varchar(255) NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL DEFAULT '0',
   `address` varchar(255) NOT NULL DEFAULT '0',
-  `phone` varchar(255) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `phone` varchar(255) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table walkwalk.user: ~0 rows (approximately)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+--
+-- Dumping data for table `user`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `address`, `phone`) VALUES
+(1, 'Ridhwan Nashir', 'd9b1d7db4cd6e70935368a1efb10e377', 'ridhwanashir@student.telkomuniversity.ac.id', 'Bandung', '087827279736'),
+(2, 'Yusril', '202cb962ac59075b964b07152d234b70', 'yusril@student.telkomuniversity.ac.id', 'Jl.Mekar Utama No.25 RT 05/04 Padasuka Cimahi', '087827279736');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bookhotel`
+--
+ALTER TABLE `bookhotel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kamar`
+--
+ALTER TABLE `kamar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penginapan`
+--
+ALTER TABLE `penginapan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tempat_wisata`
+--
+ALTER TABLE `tempat_wisata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tiketwisata`
+--
+ALTER TABLE `tiketwisata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bookhotel`
+--
+ALTER TABLE `bookhotel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kamar`
+--
+ALTER TABLE `kamar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `penginapan`
+--
+ALTER TABLE `penginapan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tempat_wisata`
+--
+ALTER TABLE `tempat_wisata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tiketwisata`
+--
+ALTER TABLE `tiketwisata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
